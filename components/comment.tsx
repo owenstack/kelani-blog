5b3b224c3ebc8021cb973d760de9de47e0e8d463
owenstack <jenukohaefobi@gmail.com>
2025-04-03 10:17:00 +0100

    Add sitemap configuration and implement comment component; update package scripts

---
+++ components/comment.tsx
@@ -0,0 +1,11 @@
+import Form from "next/form";
+import { Input } from "./ui/input";
+import { Submit } from "./submit";
+
+export function Comment() {
+	const comment = async (form: FormData) => {
+		try {
+		} catch (error) {}
+	};
+	return <div>Something</div>;
+}
