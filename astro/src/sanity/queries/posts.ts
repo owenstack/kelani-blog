a0ee19612ce5737a4dacb1e53eb91e64bd44d27b
owenstack <jenukohaefobi@gmail.com>
2025-10-09 01:33:15 +0100

    Adds dynamic post page and related posts
    Implements the dynamic page for individual posts, fetching content from the CMS based on the URL slug. This includes rendering the post body using Portable Text and styling it with the Tailwind Typography plugin.
    
    The page also displays a section for related posts based on shared tags. Additionally, the main layout is refactored to consistently wrap all page content.

---
+++ astro/src/sanity/queries/posts.ts
@@ -0,0 +1,45 @@
+import { z } from "groqd";
+import { q } from ".";
+
+export const postFromSlugQuery = q
+	.parameters<{ slug: string }>()
+	.star.filterByType("post")
+	.filterRaw("slug.current == $slug")
+	.slice(0)
+	.project((sub) => ({
+		_id: z.string(),
+		title: z.string(),
+		excerpt: z.string(),
+		content: sub.field("content[]"),
+		date: z.string(),
+		slug: sub.field("slug.current", z.string()),
+		tags: sub
+			.field("tags[]")
+			.deref()
+			.project({
+				_id: z.string(),
+				title: z.string(),
+				slug: ["slug.current", z.string()],
+			}),
+		coverImage: sub
+			.field("coverImage.asset")
+			.deref()
+			.field("url", z.string().nullable()),
+	}));
+
+export const relatedPostsQuery = q
+	.parameters<{ tags: string[]; excludeId: string }>()
+	.star.filterByType("post")
+	.filterRaw("count((tags[]._ref)[@ in $tags]) > 0 && _id != $excludeId")
+	//@ts-expect-error
+	.order("count((tags[]._ref)[@ in $tags]) desc, date desc")
+	.slice(0, 3)
+	.project((sub) => ({
+		title: z.string(),
+		slug: sub.field("slug.current", z.string()),
+		excerpt: z.string(),
+		coverImage: sub
+			.field("coverImage.asset")
+			.deref()
+			.field("url", z.string().nullable()),
+	}));
